0ffb081b477fd91e7fc7a1876c32ed3550411ec8
transparentdata243 <transparentdata243@gmail.com>
2020-06-18 08:27:39 -0700

    add other changes

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://eqsllclntl.execute-api.us-east-2.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-3kby9y3i.auth0.com',
+  clientId: 'ZBVJiXWthWlxLxTKQKVXEQ9hRUyUe94P',
   callbackUrl: 'http://localhost:3000/callback'
 }
